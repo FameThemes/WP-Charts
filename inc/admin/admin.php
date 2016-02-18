631f7c21ec6a297d69129c299a53ab63a68fa333
TruongSa <shrimp2t@gmail.com>
2016-02-18 13:14:42 +0700

    add Shortcode Attributes ref

--- inc/admin/admin.php
+++ inc/admin/admin.php
@@ -95,8 +95,11 @@ function wp_chart_submenu_page_callback() {
             white-space: -o-pre-wrap;    /* Opera 7 */
             word-wrap: break-word;       /* Internet Explorer 5.5+ */
             display: block;
-            background: #F1F1F1;
+            background: #ffffff;
             padding: 5px;
+        }
+        .inside .shortcode{
+            background: #F1F1F1;
         }
         .wp-chart-box {
             max-width: 500px;
@@ -117,7 +120,7 @@ function wp_chart_submenu_page_callback() {
     <div class="wrap">
         <h1><?php _e( 'WP Charts', 'nona' ); ?></h1>
         <div id="poststuff">
-
+            <h3><?php _e( 'Example Shortcode Usage', 'nona' ); ?></h3>
             <div class="postbox-container meta-box-sortables">
                 <?php foreach ( $doc_boxes as $box ){ ?>
                 <div class="wp-chart-box postbox closed">
@@ -132,6 +135,81 @@ function wp_chart_submenu_page_callback() {
                     </div>
                 </div>
                 <?php } ?>
+            </div>
+
+
+            <div class="postbox-container meta-box-sortables">
+                <h3><?php _e( 'All Shortcode Attributes', 'nona' ); ?></h3>
+
+<pre class="shortcode">'type'             = "pie"
+choose from pie, doughnut, radar, polararea, bar, line
+
+'title'            = "chartname"
+each chart requires a uniqe title. (note that the title should be unique on the page if you are using multiple charts on the same page)
+
+'width'            = "45%"
+optional - This sets the width of the container for the chart, and should be the only size property you need to adjust.  Setting it as a % value will make the chart fluid / responsive, you can use any valid CSS measurement of value (em, px, %).
+
+'height'           = "auto"
+optional - the height will automatticaly be proportionate to the width, and you should not need to adjust this .
+
+'canvaswidth'      = "625"
+optional - this will be converted to px, only adjust this up or down if you experience rendering issues.
+
+'canvasheight'     = "625"
+optional - this will be converted to px, only adjust this up or down if you experience rendering issues.
+
+'margin'           = "20px"
+optional - use standard css syntax for the margin, defaults to 5px for top, bottom, left and right.
+
+'align'            = "alignleft"
+optional - use one of the standard WordPress alignment classes alignleft, alignright, aligncenter.
+
+'class'            = ""
+optional - apply a css class to the chart container.
+
+'labels'           = ""
+Used for the bar, line and polararea charts.
+
+'data'             = "30,50,100"
+Used for the pie, doughnut and radar charts.
+
+'datasets'         = "30,50,100 next 20,90,75"
+Used for the bar, line, and radar charts,  the data for each 'set' is put in as before, but using the 'next' keyword to seperate each of the datasets.
+
+'colors'           = "69D2E7,#E0E4CC,#F38630,#96CE7F,#CEBC17,#CE4264"
+optional -  These should be put in in there HEX value only(as above). These are the default colors, there should be the same number of colours as data points, or datasets, but if you only got a few, or too many, don't worry the plugin will handle it.  (more practically if you don't want your chart to look like a rainbow, the plugin will cycle a set a colors for your data)
+
+'fillopacity'      = "0.7"
+optional -  for line, bar and polararea type charts you can set an opactiy for fill of the chart.
+
+'pointstrokecolor' = "#FFFFFF"
+optional -  for line and polararea type charts you can set the point color, though usually looks pretty good with the default.
+
+'animation'        => 'true'
+optional -  turn the load animation for the charts on or off
+
+'scaleFontSize'    => '12'
+optional -  adjust the font size of the scale for the charts that display it
+
+'scaleFontColor'   => '#666'
+optional -  change the scale font colour
+
+'scaleOverride'    => 'false'
+optional -  by default this is turned off, and the script attempts to output an appropriate scale, setting this to true will require the following three properties to be set as well: scaleSteps, scaleStepWidth and scaleStartValue
+
+'scaleSteps'       => 'null'
+optional -  only applicable if scaleOveride is set to true - the number of steps in the scale
+
+'scaleStepWidth'   => 'null'
+optional -  only applicable if scaleOveride is set to true - the value jump used in the scale
+
+'scaleStartValue'  => 'null'
+optional -  only applicable if scaleOveride is set to true - the center starting value for the scale
+
+Example Usage with scale options
+[wp_charts title="linechart" type="line" align="alignright" margin="5px 20px" datasets="40,43,61,50 next 33,15,40,22" labels="one,two,three,four" scaleoverride="true" scalesteps="5" scalestepwidth="10" scalestartvalue="0"]
+                </pre>
             </div>
 
 
